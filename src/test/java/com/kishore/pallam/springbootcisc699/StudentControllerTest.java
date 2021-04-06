a65fdafc7a09dd6892a34a1c9ce4bf8d769a002a
Krishna Kishore pallamreddy <krishnakishorepallamreddy@krishnas-mbp.lan>
2021-04-05 19:03:59 -0500

    updated

--- src/test/java/com/kishore/pallam/springbootcisc699/StudentControllerTest.java
+++ src/test/java/com/kishore/pallam/springbootcisc699/StudentControllerTest.java
@@ -43,7 +43,7 @@ public class StudentControllerTest {
 		
 
 		mockMvc.perform(MockMvcRequestBuilders.get("/hello")).andExpect(MockMvcResultMatchers.status().is(200))
-        .andExpect(MockMvcResultMatchers.content().string("Hello World"))
+        .andExpect(MockMvcResultMatchers.content().string("Hello World again"))
         .andExpect(MockMvcResultMatchers.header().string("Content-Type", "text/plain;charset=UTF-8"));
         //.andExpect(MockMvcResultMatchers.header().string("Content-Length", "19"));
     
